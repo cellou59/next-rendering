f064b60b5e821d06921b8dfb134a648008496aae
cellouRetailShake <cellu@retailshake.com>
2025-04-02 13:37:37 +0200

    🎸 feat: Implement cookie handling to conditionally render posts based on 'userid'

--- src/app/exercises/dynamic-rendering/cookies-headers/page.exercise.tsx
+++ src/app/exercises/dynamic-rendering/cookies-headers/page.exercise.tsx
@@ -1,26 +1,22 @@
 import RenderTime from '@/components/render-time'
 import {getPosts} from '@/db/sgbd'
 import {Post} from '@/lib/type'
-// 🐶 Importe cookies de 'next/headers'
-
-// 🤖 import {cookies} from 'next/headers'
+import {cookies} from 'next/headers'
 
 const Page = async () => {
   const posts = await getPosts()
 
   // https://nextjs.org/docs/app/api-reference/functions/cookies
   // 🐶 Recupère les cookies
-  // const cookieStore =  ...
-
-  // 🐶 Récupere la valeur de 'userid' dans 'cookieStore'
-
-  // 🐶 Si 'userid' n'est pas renseigné (pas de user), vider posts
-
+  const cookieStore = await cookies()
+  const userid = cookieStore.get('userid')
   return (
     <div className="mx-auto max-w-4xl p-6 text-lg">
       <h1 className="mb-4 text-center text-3xl font-bold">Fetch Posts</h1>
       <ul className="list-disc p-4 pl-4">
-        {posts?.map((post: Post) => <li key={post.title}>{post.title}</li>)}
+        {userid
+          ? posts?.map((post: Post) => <li key={post.title}>{post.title}</li>)
+          : ''}
       </ul>
       <RenderTime />
     </div>
