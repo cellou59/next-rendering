d47f5c5209d13829264e270864b49b00c2351adb
cellouRetailShake <cellu@retailshake.com>
2025-04-02 14:26:41 +0200

    💡 refactor: Simplify post fetching logic and comment out unused authentication methods

--- src/app/exercises/dynamic-rendering/cookies-headers/page.exercise.tsx
+++ src/app/exercises/dynamic-rendering/cookies-headers/page.exercise.tsx
@@ -1,20 +1,23 @@
 import RenderTime from '@/components/render-time'
-import {getPosts} from '@/db/sgbd'
+// import {getPosts} from '@/db/sgbd'
 import {Post} from '@/lib/type'
 //import {cookies} from 'next/headers'
-import {headers} from 'next/headers'
+// import {headers} from 'next/headers'
 
 const Page = async () => {
-  const posts = await getPosts()
-  let isAuth = false
+  const data = await fetch('https://jsonplaceholder.typicode.com/posts')
+  //pense a tester les otions pour faire un rendu statique et dynamique
+  const posts = await data.json()
+  // const posts = await getPosts()
+  const isAuth = true
   // https://nextjs.org/docs/app/api-reference/functions/cookies
   // RSC static to dynamic du to the use of cookies()
   //const cookieStore = await cookies()
   // isAuth = !!cookieStore.get('userid')
   // RSC static to dynamic du to the use of headers
-  const headersList = await headers()
-  const userAgent = headersList.get('User-Agent')
-  isAuth = !!userAgent?.includes('Chrome/134.0.0.0')
+  // const headersList = await headers()
+  // const userAgent = headersList.get('User-Agent')
+  // isAuth = !!userAgent?.includes('Chrome/134.0.0.0')
   return (
     <div className="mx-auto max-w-4xl p-6 text-lg">
       <h1 className="mb-4 text-center text-3xl font-bold">Fetch Posts</h1>
